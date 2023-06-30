8691ddc4958f7c39ac8529715a0565805269be84
Atharv <atharvpatil5177@gmail.com>
2023-06-30 14:48:50 +0530

    home page ui ready with states

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,69 @@
 import Item from "./Item";
+import itemImage1 from "./assets/1.png";
+import itemImage2 from "./assets/2.png";
+import itemImage3 from "./assets/3.png";
+import itemImage4 from "./assets/4.png";
+import itemImage5 from "./assets/5.png";
+import itemImage6 from "./assets/6.webp";
+import itemImage7 from "./assets/7.webp";
+import itemImage8 from "./assets/8.webp";
+import styled from "styled-components";
+import { useState } from "react";
 
 const Home = () => {
+  const [cartAmount, setCartAmount] = useState(0);
   return (
     <>
-      <h1>Simple Ecommerse</h1>
+      <div style={{ textAlign: "center" }}>
+        <h1>Simple Ecommerse</h1>
+        <ItemContainer>
+          <ItemRow>
+            <Item
+              itemImage={itemImage1}
+              itemName="Iphone"
+              itemPrice={500}
+              setCartAmount={setCartAmount}
+            />
+            <Item itemImage={itemImage2} itemName="Macbook" itemPrice="$999" />
+            <Item itemImage={itemImage3} itemName="Camera" itemPrice="$999" />
+          </ItemRow>
+          <ItemRow>
+            <Item
+              itemImage={itemImage4}
+              itemName="Jacket"
+              itemPrice={200}
+              setCartAmount={setCartAmount}
+            />
+            <Item
+              itemImage={itemImage5}
+              itemName="LED Strip Lights"
+              itemPrice="$999"
+            />
+            <Item
+              itemImage={itemImage6}
+              itemName="Sweat Shirt"
+              itemPrice="$999"
+            />
+          </ItemRow>
+          <ItemRow>
+            <Item itemImage={itemImage7} itemName="T-shirt" itemPrice="$999" />
+            <Item itemImage={itemImage8} itemName="Cool Cap" itemPrice="$999" />
+          </ItemRow>
+        </ItemContainer>
+        <h1>Cart Value: {cartAmount}</h1>
+      </div>
     </>
   );
 };
+
+const ItemContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const ItemRow = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default Home;
--- src/Item.jsx
+++ src/Item.jsx
@@ -1,5 +1,56 @@
-export const Item = () => {
-  return <div>Item</div>;
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+import { useState } from "react";
+
+export const Item = ({ itemImage, itemName, itemPrice, setCartAmount }) => {
+  const [quantity, setQuantity] = useState(0);
+  const handleDecrement = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+      setCartAmount((prev) => prev - itemPrice);
+    }
+  };
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+    setCartAmount((prev) => prev + itemPrice);
+  };
+  return (
+    <>
+      <ItemComponent>
+        <img src={itemImage} alt="product-image" />
+        <ItemName>{itemName}</ItemName>
+        <ItemPrice>{itemPrice}</ItemPrice>
+        <Quantity>
+          <button onClick={handleDecrement}>-</button>
+          {quantity}
+          <button onClick={handleIncrement}>+</button>
+        </Quantity>
+      </ItemComponent>
+    </>
+  );
 };
+
+const ItemComponent = styled.div`
+  margin-top: 2rem;
+  img {
+    height: 200px;
+    width: 200px;
+  }
+`;
+
+const ItemName = styled.div`
+  font-weight: bold;
+  font-size: 1.5em;
+  padding-top: 0.25rem;
+  padding-bottom: 0.25rem;
+`;
+
+const ItemPrice = styled(ItemName)``;
+
+const Quantity = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 1rem;
+`;
 
 export default Item;
